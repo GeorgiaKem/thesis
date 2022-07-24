5fdeb19801c8d9a567b41503e488b717ab1b283a
stellakaranikola <georgiakem10@gmail.com>
2022-07-24 12:29:15 +0300

    Revert "push"
    This reverts commit 8fba36dff36877418fb708713af6317266623ad5.

--- thesis-api/app/Http/Controllers/SemesterController.php
+++ thesis-api/app/Http/Controllers/SemesterController.php
@@ -3,14 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Semester;
 
 class SemesterController extends Controller
 {
-    public function index()
-    {
-        $list = Semester::all();
-
-        return response()->json($list);
-    }
+    //
 }
